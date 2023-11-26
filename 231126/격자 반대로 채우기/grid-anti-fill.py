6fced94d04c1168f4dcbfcaab2b120241efe469e
kook <105410621+Kookkool@users.noreply.github.com>
2023-11-26 21:41:41 +0900

    Title: 격자 반대로 채우기, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 231126/격자 반대로 채우기/grid-anti-fill.py
+++ 231126/격자 반대로 채우기/grid-anti-fill.py
@@ -6,45 +6,19 @@ answer=[
 
 count=1
 
-if n%2==0:
-    for start_col in range(n-1,-1,-1):
-        curr_col=start_col
-    
-        if start_col%2!=0:
-            curr_row=n-1
-            while 0<=curr_col and curr_row >= 0:
-                answer[curr_row][curr_col]=count
+for start_col in range(n-1,-1,-1):
+    if (n-1-start_col)%2==0:
+        
+        for curr_row in range(n-1,-1,-1):
+            answer[curr_row][start_col]=count
+            count+=1
 
-                curr_row-=1
-                count+=1
+    else :
+        for curr_row in range(n):
+        
+            answer[curr_row][start_col]=count
+            count+=1
 
-        else :
-            curr_row=0
-            while 0<=curr_col and curr_row < n:
-                answer[curr_row][curr_col]=count
-
-                curr_row+=1
-                count+=1
-else:
-     for start_col in range(n-1,-1,-1):
-        curr_col=start_col
-    
-        if start_col%2==0:
-            curr_row=n-1
-            while 0<=curr_col and curr_row >= 0:
-                answer[curr_row][curr_col]=count
-
-                curr_row-=1
-                count+=1
-
-        else :
-            curr_row=0
-            while 0<=curr_col and curr_row < n:
-                answer[curr_row][curr_col]=count
-
-                curr_row+=1
-                count+=1
-   
 for r in range(n):
     for c in range(n):
         print(answer[r][c],end =' ')
