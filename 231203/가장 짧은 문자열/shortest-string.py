5e1efc16960aa28a286b1f5c077aac12f3343b12
kook <105410621+Kookkool@users.noreply.github.com>
2023-12-03 17:28:43 +0900

    Title: 가장 짧은 문자열, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 231203/가장 짧은 문자열/shortest-string.py
+++ 231203/가장 짧은 문자열/shortest-string.py
@@ -6,23 +6,7 @@ w1_len=len(w1)
 w2_len=len(w2)
 w3_len=len(w3)
 
+mx = max([w1_len, w2_len, w3_len])
+mn = min([w1_len, w2_len, w3_len])
 
-if w1_len>w2_len:
-    if w1_len>w3_len:
-        max_word=w1
-        if w2_len>w3_len:
-            min_word=w3
-        else:
-            min_word=w2
-    else:
-        max_word=w3
-        min_word=w2
-else:
-    if w2_len>w3_len:
-        max_word=w2
-        min_word=w1
-    else:
-        max_word=w3
-        min_word=w1
-
-print(len(max_word)-len(min_word))
+print(mx - mn)
