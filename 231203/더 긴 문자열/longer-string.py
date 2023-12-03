813ff1c9b85792e66a40ec69922b6f5797c04a04
kook <105410621+Kookkool@users.noreply.github.com>
2023-12-03 17:30:52 +0900

    Title: 더 긴 문자열, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 231203/더 긴 문자열/longer-string.py
+++ 231203/더 긴 문자열/longer-string.py
@@ -1,6 +1,11 @@
 w1,w2=tuple(input().split())
 
-if len(w1) == len(w2):
+len1=len(w1)
+len2=len(w2)
+
+if len1 == len2:
     print('same')
+elif len1>len2:
+    print(w1,len(w1))
 else:
     print(w2,len(w2))
