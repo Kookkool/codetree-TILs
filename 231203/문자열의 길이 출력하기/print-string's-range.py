b5a2747d7defde16f220f48b6ecfc0c040ba0105
kook <105410621+Kookkool@users.noreply.github.com>
2023-12-03 17:08:40 +0900

    Title: 문자열의 길이 출력하기, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 231203/문자열의 길이 출력하기/print-string's-range.py
+++ 231203/문자열의 길이 출력하기/print-string's-range.py
@@ -1,5 +1,16 @@
-length=0
-for _ in range(2):
-    length+=len(input())
+# length=0
+# for _ in range(2):
+#     length+=len(input())
 
-print(length)
+# print(length)
+
+# 문자열을 입력받습니다.
+str1 = input()
+str2 = input()
+
+# 문자열의 길이를 구합니다.
+len1 = len(str1)
+len2 = len(str2)
+	
+# 두 문자열의 길이의 합을 출력합니다.
+print(len1 + len2)
