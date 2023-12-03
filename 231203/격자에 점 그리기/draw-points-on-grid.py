f0e98a8e09fbaf492e13aef82fdc3aef1794c2c3
kook <105410621+Kookkool@users.noreply.github.com>
2023-12-03 16:48:26 +0900

    Title: 격자에 점 그리기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231203/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,15 @@
+n,m=tuple(map(int,input().split()))
+
+matrix=[
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+for i in range(m):
+    r,c=tuple(map(int,input().split()))
+    matrix[r-1][c-1]=i+1
+
+for rr in range(n):
+    for cc in range(n):
+        print(matrix[rr][cc], end=' ')
+    print()
